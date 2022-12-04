952280372bbc2f9cbb6fba4e801778ac4251ab73
pc <vitali.sharikadze.1@btu.edu.ge>
2022-12-04 23:33:02 +0400

    Finished Project, wina leqciaze ar vikavi da imedia es unda gameketebina

---
+++ app/src/main/java/com/example/viewpage/PageAdapter.kt
@@ -0,0 +1,30 @@
+package com.example.viewpage
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+class PageAdapter(fm:FragmentManager) : FragmentPagerAdapter(fm) {
+    override fun getCount(): Int {
+        return 3;
+    }
+    override fun getItem(position: Int): Fragment {
+        when(position) {
+            0 -> { return Fragment1() }
+            1 -> { return Fragment2() }
+            2 -> { return Fragment3() }
+            else -> { return Fragment1()
+            }
+        }
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        when(position) {
+            0 -> { return "Plus" }
+            1 -> { return "Minus" }
+            2 -> { return "Mulyiply" }
+        }
+        return super.getPageTitle(position)
+    }
+
+}
